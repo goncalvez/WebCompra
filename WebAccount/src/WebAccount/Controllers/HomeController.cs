01b6d755b5ea278a15c45946054497f011a64376
goncalvez <goncalvez.vinicius@gmail.com>
2017-09-09 19:18:33 -0300

    Iniciando repositório.

---
+++ WebAccount/src/WebAccount/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAccount.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Conta()
+        {
+            ViewData["Message"] = "Cadastro de Clientes";
+
+            return View();
+        }
+
+        public IActionResult Compra()
+        {
+            ViewData["Message"] = "Cadastro de Compras";
+
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View();
+        }
+    }
+}
